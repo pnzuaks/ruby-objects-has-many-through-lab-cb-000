268d63e2eba175130064385f25e4a0e93cee5973
Patricia Nykaza <pnykaza@calstatela.edu>
2018-07-24 18:27:16 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,31 @@
+class Patient
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @appointments = []
+  end
+  
+  def self.all()
+    @@all 
+  end
+  
+  def new_appointment(doctor, date)
+    new_app = Appointment.new(self, date, doctor)
+    @appointments << new_app
+    return new_app
+  end
+  
+  def appointments()
+    Appointment.all.select do |app|
+      app.patient == self
+    end
+  end
+  
+  def doctors()
+    @appointments.collect { |app| app.doctor}
+  end
+  
+end
